8e6ecb04c34b5e39236fb4edcb696d4360d0cd51
Dylan_W <ddwalters789@gmail.com>
2023-12-21 10:20:28 -0500

    Added spawn mechanics for starting and stopping the game

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] bool gameActive;
+
     [SerializeField] GameObject ballOne;
 
     [SerializeField] GameObject balltwo;
@@ -14,6 +16,8 @@ public class Spawner : MonoBehaviour
 
     [SerializeField] float spawnRadius = 0.28f;
 
+    GameObject ballHolder;
+
     float waitingTime;
 
     float decrementTime = 0.001f;
@@ -23,11 +27,19 @@ public class Spawner : MonoBehaviour
 
     void Start()
     {
+        gameActive = false;
+        ballHolder = new GameObject("GameBallHolder");
         waitingTime = Random.Range(lowSpawnSpeed, HighSpawnSpeed);
     }
 
     void Update()
     {
+        if (gameActive){
+            SpawnBalls();
+        }
+    }
+
+    private void SpawnBalls(){
         timer += Time.deltaTime;
 
         if (lowSpawnSpeed < -0.2f && HighSpawnSpeed > 0.2f)
@@ -47,14 +59,21 @@ public class Spawner : MonoBehaviour
 
             int randomBall = Random.Range(0, 100);
             if (randomBall < 50)
-                Instantiate(ballOne, position, rotation);
+                Instantiate(ballOne, position, rotation, ballHolder.transform);
             else if (randomBall < 85)
-                Instantiate(balltwo, position, rotation);
+                Instantiate(balltwo, position, rotation, ballHolder.transform);
             else
-                Instantiate(ballThree, position, rotation);
+                Instantiate(ballThree, position, rotation, ballHolder.transform);
 
             waitingTime = Random.Range(lowSpawnSpeed, HighSpawnSpeed);
             timer = 0;
         }
+    }
+
+    public void setGameActive() {
+        // remove previous balls here --
+        Destroy(ballHolder);
+        ballHolder = new GameObject("GameBallHolder");
+        gameActive = true;
     }
 }
